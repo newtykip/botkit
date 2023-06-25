2f0d96a5d10eb07fc88e6e48cf47f8279f2cb5c4
newtykins <newtykip@outlook.com>
2023-06-25 17:14:13 +0100

    chore: actually like export Embed that could be cool
    omd

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import Listener from './structures/Listener';
 import Precondition from './structures/Precondition';
 import Command from './structures/Command';
 import { Logger } from './structures/Logger';
+import Embed from './structures/Embed';
 
-export { Client, Listener, Precondition, Command, Logger };
+export { Client, Listener, Precondition, Command, Logger, Embed };
